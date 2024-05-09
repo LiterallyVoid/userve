2e873b07d816e69ce298a35725b72eb5d4939196
literallyvoid <literallyvoid@gmail.com>
2024-05-09 13:10:50 -0700

    Increase `read` chunk size while reading HTTP requests

--- src/main.c
+++ src/main.c
@@ -100,7 +100,7 @@ int main(int argc, char **argv) {
 		http_parser_init(&parser);
 
 		while (true) {
-			uint8_t buffer[16];
+			uint8_t buffer[512];
 			ssize_t recv_result = recv(connection.fd, &buffer, sizeof(buffer), 0);
 			if (recv_result == -1) {
 				perror("read");
