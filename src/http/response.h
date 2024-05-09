b6546754636ef17dbc50ff21a37c0dc371379d6f
literallyvoid <literallyvoid@gmail.com>
2024-05-09 10:18:18 -0700

    Add comments

--- src/http/response.h
+++ src/http/response.h
@@ -9,7 +9,10 @@ typedef struct {
 	Buffer headers;
 } HttpResponse;
 
+// Initialize `self`, in preparation for writing an HTTP response to `write_fd`.
 void http_response_init(HttpResponse *self, int write_fd);
+
+// If headers haven't been sent yet, send 500 Internal Server Error in response.
 void http_response_deinit(HttpResponse *self);
 
 void http_response_add_header(HttpResponse *res, Slice header, Slice foo);
