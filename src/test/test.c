0201ca6affd8037e580b07e10293ef98d2bc2765
literallyvoid <literallyvoid@gmail.com>
2024-05-09 15:34:40 -0700

    Improve tests

--- src/test/test.c
+++ src/test/test.c
@@ -8,17 +8,25 @@
 static void test_arguments(void) {
 	Arguments arguments;
 
-	arguments_parse(&arguments, 2, (const char*[]) { "--address", "--address", "foobar" });
+	arguments_parse(&arguments, 2, (const char*[]) { "@test0", "--address", "foobar" });
 	assert(strcmp(arguments.address, "localhost") == 0);
 
-	arguments_parse(&arguments, 3, (const char*[]) { "--address", "--address", "foobar" });
+	arguments_parse(&arguments, 3, (const char*[]) { "@test1", "--address", "foobar" });
 	assert(strcmp(arguments.address, "foobar") == 0);
 
-	arguments_parse(&arguments, 2, (const char*[]) { "--address", "--address=1234" });
+	arguments_parse(&arguments, 2, (const char*[]) { "@test2", "--address=1234" });
 	assert(strcmp(arguments.address, "1234") == 0);
 
-	arguments_parse(&arguments, 3, (const char*[]) { "--address", "-a", "--address" });
+	arguments_parse(&arguments, 3, (const char*[]) { "@test3", "-a", "--address" });
 	assert(strcmp(arguments.address, "--address") == 0);
+
+	arguments_parse(&arguments, 3, (const char*[]) { "@test4", "-p", "-t" });
+	assert(strcmp(arguments.port, "-t") == 0);
+	assert(!arguments.test);
+
+	arguments_parse(&arguments, 4, (const char*[]) { "@test5", "-p", "", "-t" });
+	assert(strcmp(arguments.port, "") == 0);
+	assert(arguments.test);
 }
 
 void test_all(void) {
