8e3c8804914f997c0f9602987f0b8375d06f7b27
literallyvoid <literallyvoid@gmail.com>
2024-05-09 16:25:14 -0700

    Add context to all `expects` (not compiling)

--- src/test/test.c
+++ src/test/test.c
@@ -20,24 +20,24 @@ static void test_arguments(void) {
 	Arguments arguments;
 
 	arguments_parse(&arguments, 2, (const char*[]) { "@test0", "--address", "foobar" });
-	expect(strcmp(arguments.address, "localhost") == 0);
+	EXPECT(ctx, strcmp(arguments.address, "localhost") == 0);
 
 	arguments_parse(&arguments, 3, (const char*[]) { "@test1", "--address", "foobar" });
-	expect(strcmp(arguments.address, "foobar") == 0);
+	EXPECT(ctx, strcmp(arguments.address, "foobar") == 0);
 
 	arguments_parse(&arguments, 2, (const char*[]) { "@test2", "--address=1234" });
-	expect(strcmp(arguments.address, "1234") == 0);
+	EXPECT(ctx, strcmp(arguments.address, "1234") == 0);
 
 	arguments_parse(&arguments, 3, (const char*[]) { "@test3", "-a", "--address" });
-	expect(strcmp(arguments.address, "--address") == 0);
+	EXPECT(ctx, strcmp(arguments.address, "--address") == 0);
 
 	arguments_parse(&arguments, 3, (const char*[]) { "@test4", "-p", "-t" });
-	expect(strcmp(arguments.port, "-t") == 0);
-	expect(!arguments.test);
+	EXPECT(ctx, strcmp(arguments.port, "-t") == 0);
+	EXPECT(ctx, !arguments.test);
 
 	arguments_parse(&arguments, 4, (const char*[]) { "@test5", "-p", "", "-t" });
-	expect(strcmp(arguments.port, "") == 0);
-	expect(arguments.test);
+	EXPECT(ctx, strcmp(arguments.port, "") == 0);
+	EXPECT(ctx, arguments.test);
 }
 
 #include "../buffer.h"
@@ -48,9 +48,9 @@ static void test_buffer(void) {
 
 	buffer_concat(&buffer, slice_from_cstr("12345"));
 	buffer_concat(&buffer, slice_from_cstr("678"));
-	expect(buffer.len == 8);
-	expect(buffer_slice(&buffer).len == 8);
-	expect(memcmp(buffer.bytes, "12345678", 8) == 0);
+	EXPECT(ctx, buffer.len == 8);
+	EXPECT(ctx, buffer_slice(&buffer).len == 8);
+	EXPECT(ctx, memcmp(buffer.bytes, "12345678", 8) == 0);
 
 	buffer_deinit(&buffer);
 
@@ -58,7 +58,7 @@ static void test_buffer(void) {
 	buffer_init(&buffer);
 
 	buffer_concat_printf(&buffer, "hey %d", 123);
-	expect(slice_equal(buffer_slice(&buffer), slice_from_cstr("hey 123")));
+	EXPECT(ctx, slice_equal(buffer_slice(&buffer), slice_from_cstr("hey 123")));
 
 	buffer_deinit(&buffer);
 
@@ -66,29 +66,29 @@ static void test_buffer(void) {
 	buffer_init(&buffer);
 
 	buffer_reserve_total(&buffer, 10);
-	expect(buffer_slice(&buffer).len == 0);
-	expect(buffer_uninitialized(&buffer).len >= 10);
+	EXPECT(ctx, buffer_slice(&buffer).len == 0);
+	EXPECT(ctx, buffer_uninitialized(&buffer).len >= 10);
 
 	buffer_reserve_total(&buffer, 1000);
-	expect(buffer_slice(&buffer).len == 0);
-	expect(buffer_uninitialized(&buffer).len >= 1000);
+	EXPECT(ctx, buffer_slice(&buffer).len == 0);
+	EXPECT(ctx, buffer_uninitialized(&buffer).len >= 1000);
 
 	buffer_reserve_additional(&buffer, 1020);
-	expect(buffer_slice(&buffer).len == 0);
-	expect(buffer_uninitialized(&buffer).len >= 1020);
+	EXPECT(ctx, buffer_slice(&buffer).len == 0);
+	EXPECT(ctx, buffer_uninitialized(&buffer).len >= 1020);
 
 	for (int i = 0; i < 102; i++) {
 		buffer_concat(&buffer, slice_from_cstr("123456789A"));
 	}
-	expect(buffer_slice(&buffer).len == 1020);
+	EXPECT(ctx, buffer_slice(&buffer).len == 1020);
 
 	buffer_clear(&buffer);
-	expect(buffer_slice(&buffer).len == 0);
-	expect(buffer_uninitialized(&buffer).len >= 1020);
+	EXPECT(ctx, buffer_slice(&buffer).len == 0);
+	EXPECT(ctx, buffer_uninitialized(&buffer).len >= 1020);
 
 	buffer_clear_capacity(&buffer);
-	expect(buffer_slice(&buffer).len == 0);
-	expect(buffer_uninitialized(&buffer).len == 0);
+	EXPECT(ctx, buffer_slice(&buffer).len == 0);
+	EXPECT(ctx, buffer_uninitialized(&buffer).len == 0);
 
 	buffer_deinit(&buffer);
 }
@@ -96,20 +96,20 @@ static void test_buffer(void) {
 static void test_slice(void) {
 	Slice a = slice_from_cstr("abc xyz def");
 
-	expect(slice_equal(a, slice_from_cstr("abc xyz def")));
-	expect(!slice_equal(a, slice_from_cstr("abc xyz")));
-	expect(!slice_equal(a, slice_from_cstr("abc xyz defg")));
-	expect(!slice_equal(a, slice_from_cstr("12345678")));
+	EXPECT(ctx, slice_equal(a, slice_from_cstr("abc xyz def")));
+	EXPECT(ctx, !slice_equal(a, slice_from_cstr("abc xyz")));
+	EXPECT(ctx, !slice_equal(a, slice_from_cstr("abc xyz defg")));
+	EXPECT(ctx, !slice_equal(a, slice_from_cstr("12345678")));
 
-	expect(slice_equal(a, slice_from_len((uint8_t*) "abc xyz def zyx", 11)));
+	EXPECT(ctx, slice_equal(a, slice_from_len((uint8_t*) "abc xyz def zyx", 11)));
 
-	expect(slice_equal(slice_remove_start(a, 0), a));
-	expect(slice_equal(slice_remove_start(a, 4), slice_from_cstr("xyz def")));
-	expect(slice_equal(slice_remove_start(a, 11), slice_new()));
+	EXPECT(ctx, slice_equal(slice_remove_start(a, 0), a));
+	EXPECT(ctx, slice_equal(slice_remove_start(a, 4), slice_from_cstr("xyz def")));
+	EXPECT(ctx, slice_equal(slice_remove_start(a, 11), slice_new()));
 
-	expect(slice_equal(slice_keep_bytes_from_end(a, 0), slice_from_cstr("")));
-	expect(slice_equal(slice_keep_bytes_from_end(a, 2), slice_from_cstr("ef")));
-	expect(slice_equal(slice_keep_bytes_from_end(a, 11), a));
+	EXPECT(ctx, slice_equal(slice_keep_bytes_from_end(a, 0), slice_from_cstr("")));
+	EXPECT(ctx, slice_equal(slice_keep_bytes_from_end(a, 2), slice_from_cstr("ef")));
+	EXPECT(ctx, slice_equal(slice_keep_bytes_from_end(a, 11), a));
 }
 
 #include "../http/parser.h"
@@ -336,27 +336,27 @@ static void test_http_parser(void) {
 
 		switch (cases[i].result) {
 		case GOOD: {
-			expect(err == ERR_SUCCESS);
-			expect(result.done);
+			EXPECT(ctx, err == ERR_SUCCESS);
+			EXPECT(ctx, result.done);
 
 			Slice trailing = slice_new();
 			if (cases[i].trailing != NULL) {
 				trailing = slice_from_cstr(cases[i].trailing);
 			}
 
-			expect(slice_equal(result.remainder_slice, trailing));
+			EXPECT(ctx, slice_equal(result.remainder_slice, trailing));
 			break;
 		}
 
 		case INCOMPLETE:
-			expect(err == ERR_SUCCESS);
-			expect(!result.done);
-			expect(cases[i].trailing == NULL);
+			EXPECT(ctx, err == ERR_SUCCESS);
+			EXPECT(ctx, !result.done);
+			EXPECT(ctx, cases[i].trailing == NULL);
 			break;
 
 		case BAD:
-			expect(err == ERR_PARSE_FAILED);
-			expect(cases[i].trailing == NULL);
+			EXPECT(ctx, err == ERR_PARSE_FAILED);
+			EXPECT(ctx, cases[i].trailing == NULL);
 			break;
 		}
 
