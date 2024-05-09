ff95b9c4f859e62710e5b96d7682f81d3c6f3a4d
literallyvoid <literallyvoid@gmail.com>
2024-05-09 10:20:06 -0700

    HTTP API exploration

--- src/http/response.h
+++ src/http/response.h
@@ -2,6 +2,17 @@
 
 #include "../buffer.h"
 
+// Long ago, the four nations lived in harmony.
+typedef enum HttpStatus {
+	HTTP_OK = 200,
+
+	HTTP_BAD_REQUEST = 400,
+
+	HTTP_NOT_FOUND = 404,
+
+	HTTP_INTERNAL_SERVER_ERROR = 500,
+} HttpStatus;
+
 typedef struct {
 	// A file descriptor to write the response to.
 	int write_fd;
@@ -15,4 +26,7 @@ void http_response_init(HttpResponse *self, int write_fd);
 // If headers haven't been sent yet, send 500 Internal Server Error in response.
 void http_response_deinit(HttpResponse *self);
 
-void http_response_add_header(HttpResponse *res, Slice header, Slice foo);
+void http_response_set_status(HttpResponse *self, HttpStatus status);
+void http_response_add_header(HttpResponse *self, Slice header, Slice foo);
+
+void http_response_end(HttpResponse *self);
