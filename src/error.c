e43d28b6aed78cd42e3466f4fbfd730592d89676
literallyvoid <literallyvoid@gmail.com>
2024-05-09 10:36:39 -0700

    Add src/error.{c.h}

---
+++ src/error.c
@@ -0,0 +1,17 @@
+#include "error.h"
+
+const char *error_to_string(Error self) {
+	switch (self) {
+	case ERR_SUCCESS:	return "(success)";
+	case ERR_UNKNOWN:	return "unknown error";
+	case ERR_OUT_OF_MEMORY:	return "out of memory";
+	case ERR_NO_SPACE:	return "no space left";
+	case ERR_PARSE_FAILED:	return "parse failed";
+	}
+
+	if (self > 0) {
+		return "(unrecognized success value)";
+	}
+
+	return "(unrecognized error)";
+}
